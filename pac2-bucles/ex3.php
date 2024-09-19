856cdb116621c9b17ae931086eb4e0987afc3226
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2024-09-19 18:42:16 +0000

    actu

---
+++ pac2-bucles/ex3.php
@@ -0,0 +1,46 @@
+<?php 
+
+echo "<h1>Ejercicio 3: numeros Pares del 50 al 500</h1>";
+echo "<div class = marco>";
+$aleatorio;
+$i=(rand(0,100));
+if ($numero % 2==0){
+    echo "El $i es par";
+    }else{
+    echo "El $i es impar";
+    }
+echo"</div>";
+?>
+
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio 3 </title>
+
+    <style>
+       h1{
+            text-align: center;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        }
+        .marco{
+            text-align: center;
+            font-size: 50px
+            
+        }
+        .impar{
+            font-style: italic;
+
+
+
+        }
+
+
+
+    </style>
+</head>
+<body>
+
+    
+</body>
+</html>
